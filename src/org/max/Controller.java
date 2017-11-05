f903b97d48cf077f46db680c50dc642bf1716a84
Linukso1D <maxxl2012@gmail.com>
2017-11-05 14:56:44 +0300

    loading image

--- src/org/max/Controller.java
+++ src/org/max/Controller.java
@@ -1,11 +1,51 @@
 package org.max;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Controller {
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-       event.getTarget();
+    private void loadImage(ActionEvent event) {
+       System.out.println("LoadImage event: ");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            try {
+                BufferedImage bufferedImage = ImageIO.read(selectedFile);
+                Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                ((ImageView) Main.mainView.lookup("#originImage")).setImage(image);
+            } catch (IOException ignored) { }
+        }
+    }
+    @FXML
+    private void exportSvg(ActionEvent event) {
+        System.out.println("ExportSvg event: ");
+    }
+    @FXML
+    private void saveData(ActionEvent event) {
+        System.out.println("SaveImage event: ");
+
+    }
+    @FXML
+    private void loadData(ActionEvent event) {
+        System.out.println("LoadData event: ");
+    }
+    @FXML
+    private void start(ActionEvent event) {
+        System.out.println("Start event: ");
     }
 }
--- src/org/max/Main.java
+++ src/org/max/Main.java
@@ -7,12 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Scene mainView;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Sorokin genetic transform to vector graphic");
-        primaryStage.setScene(new Scene(root));
+        mainView = new Scene(root);
+        primaryStage.setScene(mainView);
         primaryStage.show();
     }
 
