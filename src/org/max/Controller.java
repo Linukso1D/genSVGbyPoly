b1197190e51140dd192e8186383975c9d3fbc610
Linukso1D <maxxl2012@gmail.com>
2017-11-05 14:07:47 +0300

    add some form

---
+++ src/org/max/Controller.java
@@ -0,0 +1,11 @@
+package org.max;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+       event.getTarget();
+    }
+}
